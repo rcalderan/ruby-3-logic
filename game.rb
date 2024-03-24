e6e3ec82a322a8f278138b17040dcc2733fc4e21
Richard Calderan <richardcck@hotmail.com>
2024-03-24 17:51:50 -0300

    Colision and tests

--- game.rb
+++ game.rb
@@ -1,8 +1,7 @@
 require_relative 'ui'
 
 def readMap(number)
-    arquive = "map#{number}.txt" #/maps/map#{number}.txt <- not working on VScode, must set the complete path or configure it
-
+    arquive = "maps/map#{number}.txt" 
 
   if File.exist?(arquive)
     text = File.read(arquive)
@@ -25,31 +24,42 @@ def findHero(map)
   end
 end
 
+def calculatePos(pos, direction, map)
+  newPos = [pos[0],pos[1]]
+  case direction
+    when "w"      
+      newPos[0]-= 1  
+    when "s"
+      newPos[0]+= 1
+    when "a"
+      newPos[1]-= 1
+    when "d"
+      newPos[1]+= 1
+  end
+  return newPos
+end
+
 def play(name)
   map = readMap 1
   while true
     writeMap map
     direction = move
     hero = findHero map
-    map[hero[0]][hero[1]] = " "
-    case direction
-        when "w"
-          hero[0]-= 1
-        when "s"
-          hero[0]+= 1
-        when "a"
-          hero[1]-= 1
-        when "d"
-          hero[1]+= 1
+    newHeroPos = calculatePos(hero, direction, map)
+    if !isValidPos(newHeroPos, map)
+      next
     end
-    map[hero[0]][hero[1]] = "H"
+    #render hero
+    map[hero[0]][hero[1]] = " "  
+    hero = [newHeroPos[0],newHeroPos[1]]
+    map[hero[0]][hero[1]] = "H"   
   end
-
 end
 
-def renderHero
-
+def isValidPos(pos, map) 
+  return (pos[0] >= 0 && pos[0] < map.size && pos[1]>= 0 && pos[1]< map[0].size) && map[pos[0]][pos[1]] != "X"
 end
+
 
 def init_game
   name= welcome
--- test.rb
+++ test.rb
@@ -1,3 +1,7 @@
+# Importa as bibliotecas
+require 'csv'
+require 'json'
+
 def testFor
   size = 5
   (0..size).each do |line|
@@ -24,4 +28,56 @@ def testArrayMethods
 
 end
 
-testArrayMethods
+def read
+  text = File.read("maps/map1.txt")
+  map = text.split("\n")
+  puts text.lines[1][12]
+end
+
+read
+
+def readCsvCrude
+  csv = File.read("maps/teste.csv")
+  headers = csv.lines[0].split(",")
+  rows =[]
+  # i=1
+  (1..csv.count("\n")-1).each do |i|
+    rows[i] =  csv.lines[i].split(",")
+    
+    # rows[i] =line.split(",")
+    # i+=1
+  end
+  puts rows
+  
+  # puts rows
+  
+end
+
+def readCsv
+  csvData = CSV.read('maps/teste.csv', headers: true)
+  #puts csvData.class
+
+  jsonData = { persons: []}
+  puts jsonData
+  jsonData.persons << "2"
+  puts jsonData
+  csvData.each do |row|
+    person = {}
+    row.each do |key, value|
+      #puts "Key #{key}; Value:#{value}"
+      person[key] = value
+    end
+    #puts person['hobbies']
+    #jsonData['persons'] << person
+    # if person != nil
+    #   jsonData['persons'] << person
+      
+    # end
+  end
+
+  #File.write('test.json', JSON.pretty_generate(json_data))
+end
+
+readCsv
+
+#testArrayMethods
